42454fcc6d6e2095bbccfc3eab3d847ad22d4d24
SebAche <sebh35@gmail.com>
2017-02-01 22:25:49 +0100

    Modif du contact sur la branche aux

--- contact.php
+++ contact.php
@@ -1,3 +1,5 @@
 <?php
 
-echo("Ici le code pour le formulaire de contact");
+echo("Ici le code pour le controller du formulaire de contact");
+
+// Il s'agit surtout de générer un conflit entre 2 branches pour s"entrainer à résoudre le conflit ! ^^
