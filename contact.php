84f02c0694ec2d0f3125f0fcc2e7479ee089a374
SebAche <sebh35@gmail.com>
2017-02-01 22:45:17 +0100

    Ajout du code pour le ContactController / IndexAction

--- contact.php
+++ contact.php
@@ -1,5 +1,70 @@
 <?php
+namespace ContactBundle\Controller;
 
-echo("Ici le code pour le controller du formulaire de contact");
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
-// Il s'agit surtout de générer un conflit entre 2 branches pour s"entrainer à résoudre le conflit ! ^^
+class ContactController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+        $sujetManager = $this->get('contact.manager.sujet');
+        $listSujet = $sujetManager->getListSujet();
+        $currentActivite = $this->get('common.manager.activite')->getCurrentActivite();
+        $defaultData = array('activite' => $currentActivite);
+        if (count($listSujet) === 1) {
+            $defaultData['sujet'] = $listSujet[0];
+        }
+
+        $defaultOptions = array();
+
+        $data = $this->getContactFormData($defaultData);
+        $options = $this->getContactFormOptions($defaultOptions);
+
+        $form = $this->createForm($this->get('contact.form.type.contact'), $data, $options);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $formData = $this->formatageData($form->getData());
+            $this->get('contact.envoiMail')->sendContactMessage($formData);
+            $request->getSession()->getFlashBag()->add('info', $this->get('translator')->trans('messages.email_succes', array(), 'ContactBundle'));
+
+            return $this->redirect($this->generateUrl('formulaire_contact'));
+        }
+
+        return $this->view($form);
+    }
+
+    protected function getContactFormData($defaultData)
+    {
+        return $defaultData;
+    }
+
+    protected function getContactFormOptions($defaultOptions)
+    {
+        return $defaultOptions;
+    }
+
+    protected function formatageData($dataFormulaire)
+    {
+
+        $formatedData = array();
+
+        $formatedData['expediteur']['mail'] =$dataFormulaire['email'];
+        $formatedData['sujet'] = $dataFormulaire['sujet'];
+        $formatedData['message'] = $dataFormulaire['message'];
+        if (isset($dataFormulaire['documentContactForm'])){
+            $formatedData['uploadedFile'] = $dataFormulaire['documentContactForm'];
+        }
+
+        return $formatedData;
+    }
+
+    protected function view($form)
+    {
+        return $this->render('ContactBundle:Contact:formulaireContact.html.twig', array(
+            'form' =>$form->createView()
+        ));
+    }
+}
